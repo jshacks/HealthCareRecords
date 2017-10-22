393437badc2a8a21a1dec82c352c9fdb00391b91
Abel <alexandruionascu.bd@gmail.com>
2017-10-22 13:24:30 +0300

    added solidity contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,126 @@
 import * as React from 'react';
 import './App.css';
-import {Table, Navbar, NavItem} from 'react-materialize';
 
 
-interface Prop {
+import {
+  Table,
+  Navbar,
+  NavItem,
+  Row,
+  Col,
+  Chip,
+  Tag
+} from 'react-materialize';
 
-}
+interface Prop {}
 
 interface State {
-
 }
 
+const samples = [
+  {
+    Sodium: 135.7,
+    Potassium: 3.5,
+    Glucose: 54.3,
+    Cholesterol: 35.5,
+    Calcium: 2.4,
+  },
+  {
+    Sodium: 145.2,
+    Potassium: 5.2,
+    Glucose: 96.7,
+    Cholesterol: 6.3,
+    Calcium: 2.9
+  },
+  {
+    Sodium: 170.9,
+    Potassium: 2.4,
+    Glucose: 58.8,
+    Cholesterol: 5.1,
+    Calcium: 1.2
+  },
+  {
+    Sodium: 140.4,
+    Potassium: 5.3,
+    Glucose: 105.7,
+    Cholesterol: 6.2,
+    Calcium: 2.3
+  }
+];
 
-class App extends React.Component<Prop, State> {
-  
+class App extends React.Component < Prop,
+State > {
+
+  constuctor() {
+
+  }
+
+  getTag() {
+    let items = ["low", "normal", "high", "good"];
+    return items[Math.floor(Math.random()*items.length)];
+  }
+
+  getTableHeader(data) {
+    if (data.length == 0) {
+      return
+    }
+    let headers : Array < JSX.Element > = [];
+    for (let header of Object.keys(data[0])) {
+      headers.push(
+        <th>{header}</th>
+      );
+    }
+
+    return headers;
+  }
+
+  getTableRows(data) {
+    let tableRows : Array < JSX.Element > = [];
+    for (let row of data) {
+      let entries : Array < JSX.Element > = [];
+      for (let index in row) {
+        entries.push(
+          <td>
+            <Row>
+              <Col s={12}>
+                <Chip>
+                  <img src='https://image.flaticon.com/icons/svg/150/150654.svg' />
+                  {row[index]}
+                </Chip>
+                <Tag>{this.getTag()}</Tag>
+              </Col>
+            </Row>
+            
+          </td>
+        );
+      }
+
+      tableRows.push(
+        <tr>{entries}</tr>
+      );
+    }
+
+    return tableRows;
+  }
+
   render() {
     return (
       <div>
         <Navbar brand='Healthcare Records in Blockchain' right>
-          <NavItem href='get-started.html'>Getting started</NavItem>
-          <NavItem href='components.html'>Components</NavItem>
+          <NavItem href='get-started.html'>Powered by Ethereum Blockchain</NavItem>
         </Navbar>
 
-       <Table>
-        <thead>
-          <tr>
-            <th data-field="id">Sodium</th>
-            <th data-field="name">Potasium</th>
-            <th data-field="price">Investigation</th>
-            <th>Glucose</th>
-            <th>Creatinine</th>
-          </tr>
-        </thead>
+        <Table>
+          <thead>
+            <tr>
+              {this.getTableHeader(samples)}
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-          </tr>
-        </tbody>
-      </Table>
+          <tbody>
+            {this.getTableRows(samples)}
+          </tbody>
+        </Table>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,52 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import Web3 from 'web3';
+
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+console.log(web3.eth.accounts);
+let browser_ballot_sol_healthcareContract = web3
+  .eth
+  .contract([
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "getData",
+      "outputs": [
+        {
+          "name": "url",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]);
+
+browser_ballot_sol_healthcareContract.new({
+  from: web3.eth.accounts[0],
+  data: '0x6060604052341561000f57600080fd5b6101578061001e6000396000f300606060405260043610' +
+      '610041576000357c0100000000000000000000000000000000000000000000000000000000900463' +
+      'ffffffff1680633bc5de3014610046575b600080fd5b341561005157600080fd5b6100596100d456' +
+      '5b6040518080602001828103825283818151815260200191508051906020019080838360005b8381' +
+      '101561009957808201518184015260208101905061007e565b50505050905090810190601f168015' +
+      '6100c65780820380516001836020036101000a031916815260200191505b50925050506040518091' +
+      '0390f35b6100dc610117565b6040805190810160405280600a81526020017f676f6f676c652e636f' +
+      '6d00000000000000000000000000000000000000000000815250905090565b602060405190810160' +
+      '4052806000815250905600a165627a7a7230582098f507025a793ff11da310efdc632a2c3696dbc7' +
+      'c7224bb2592f26d61b8f06b40029',
+  gas: '4700000'
+}, function (e, contract) {
+
+  if (typeof contract.address !== 'undefined') {
+    console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+  }
+
+  console.log(contract);
+
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+  <App/>, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
